fc540c23b2a2fcd50e155740957df7186dae5b54
Anton Ovchinnikov <t2da@mail.ru>
2021-01-04 17:46:48 +0300

    first commit

---
+++ src/main/java/q/Calculator.java
@@ -0,0 +1,19 @@
+package q;
+
+public class Calculator {
+
+    public double monthlyPayment(int term, double rate, int credit) {
+        double result = 0;
+        return result;
+    }
+
+    public double total(int term, double rate, int credit) {
+        double result = 0;
+        return result;
+    }
+
+    public double overpayment(int term, double rate, int credit) {
+        double result = 0;
+        return result;
+    }
+}
